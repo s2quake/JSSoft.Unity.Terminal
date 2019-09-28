ff179a418c5d40ed2132f92b69d11791da7ec7ea
s2quake <s2quake@naver.com>
2019-09-28 10:12:24 +0900

    작업중

--- Assets/JSSoft Terminal/Scripts/CompositionRenderer.cs
+++ Assets/JSSoft Terminal/Scripts/CompositionRenderer.cs
@@ -32,12 +32,28 @@ namespace JSSoft.UI
 {
     public class CompositionRenderer : MaskableGraphic
     {
+        private static readonly int[] backgroundTriangles = new int[6] { 0, 1, 2, 2, 3, 0 };
+        private static readonly int[] foregroundTriangles = new int[6] { 4, 5, 6, 6, 7, 4 };
+
         [SerializeField]
         private TMP_FontAsset fontAsset;
 
         private char character;
         private Texture texture;
         private Material material;
+        private Vector3[] vertices = new Vector3[8];
+        private Vector2[] uvs = new Vector2[8];
+        private Color32[] colors = new Color32[8]
+        {
+            TerminalColors.Green,
+            TerminalColors.Green,
+            TerminalColors.Green,
+            TerminalColors.Green,
+            TerminalColors.Black,
+            TerminalColors.Black,
+            TerminalColors.Black,
+            TerminalColors.Black
+        };
 
         public char Character
         {
@@ -50,12 +66,21 @@ namespace JSSoft.UI
             }
         }
 
+        public TMP_FontAsset FontAsset
+        {
+            get => this.fontAsset;
+            set
+            {
+                this.fontAsset = value;
+            }
+        }
+
         public override Texture mainTexture => this.texture;
 
         protected override void OnEnable()
         {
             base.OnEnable();
-            this.material = new Material (Shader.Find("Unlit/Color"));
+            this.material = new Material(Shader.Find("Unlit/Color"));
             this.material.color = new Color(1, 0, 1, 1);
             this.SetVerticesDirty();
         }
@@ -63,74 +88,6 @@ namespace JSSoft.UI
         protected override void OnPopulateMesh(VertexHelper vh)
         {
             base.OnPopulateMesh(vh);
-
-            // var vertices = new UIVertex[8];
-            // var rect = this.rectTransform.rect;
-
-            // vertices[0].color = base.color;
-            // vertices[1].color = base.color;
-            // vertices[2].color = base.color;
-            // vertices[3].color = base.color;
-            // vertices[4].color = TerminalColors.Black;
-            // vertices[5].color = TerminalColors.Black;
-            // vertices[6].color = TerminalColors.Black;
-            // vertices[7].color = TerminalColors.Black;
-
-            // var fontAsset = FontUtility.GetFontAsset(this.fontAsset, this.character);
-            // if (fontAsset != null)
-            // {
-            //     var characterInfo = fontAsset.characterLookupTable[this.character];
-            //     var texture = fontAsset.atlasTexture;
-            //     var glyph = characterInfo.glyph;
-            //     var glyphRect = glyph.glyphRect;
-            //     var textWidth = (float)texture.width;
-            //     var textHeight = (float)texture.height;
-            //     var uv0 = new Vector2(glyphRect.x / textWidth, glyphRect.y / textHeight);
-            //     var uv1 = new Vector2((glyphRect.x + glyphRect.width) / textWidth, (glyphRect.y + glyphRect.height) / textHeight);
-            //     var x = (rect.x + rect.width / 2) - (glyphRect.width / 2);
-            //     var y = (rect.y + rect.height / 2) - (glyphRect.height / 2);
-
-            //     vertices[0].position = new Vector3(x, y, 0);
-            //     vertices[1].position = new Vector3(x, y + glyphRect.height, 0);
-            //     vertices[2].position = new Vector3(x + glyphRect.width, y + glyphRect.height, 0);
-            //     vertices[3].position = new Vector3(x + glyphRect.width, y, 0);
-
-            //     vertices[0].uv0 = new Vector2(uv0.x, uv0.y);
-            //     vertices[1].uv0 = new Vector2(uv0.x, uv1.y);
-            //     vertices[2].uv0 = new Vector2(uv1.x, uv1.y);
-            //     vertices[3].uv0 = new Vector2(uv1.x, uv0.y);
-            //     this.texture = texture;
-            // }
-            // else
-            // {
-            //     vertices[0].position = new Vector3(rect.x + 100, rect.y, 0);
-            //     vertices[1].position = new Vector3(rect.x + 100, rect.y + rect.height, 0);
-            //     vertices[2].position = new Vector3(rect.x - 100 + rect.width, rect.y + rect.height, 0);
-            //     vertices[3].position = new Vector3(rect.x - 100 + rect.width, rect.y, 0);
-            //     vertices[4].position = new Vector3(rect.x + 200, rect.y + 100, 0);
-            //     vertices[5].position = new Vector3(rect.x + 200, rect.y + rect.height - 100, 0);
-            //     vertices[6].position = new Vector3(rect.x - 200 + rect.width, rect.y + rect.height - 100, 0);
-            //     vertices[7].position = new Vector3(rect.x - 200 + rect.width, rect.y + 100, 0);
-
-            //     vertices[0].uv0 = new Vector2(0, 0);
-            //     vertices[1].uv0 = new Vector2(0, 0);
-            //     vertices[2].uv0 = new Vector2(0, 0);
-            //     vertices[3].uv0 = new Vector2(0, 0);
-            //     vertices[4].uv0 = new Vector2(0, 0);
-            //     vertices[5].uv0 = new Vector2(0, 1);
-            //     vertices[6].uv0 = new Vector2(1, 1);
-            //     vertices[7].uv0 = new Vector2(1, 0);
-            //     this.texture = this.fontAsset?.atlasTexture;
-            // }
-            // vh.Clear();
-            // for (var i = 0; i < vertices.Length; i++)
-            // {
-            //     vh.AddVert(vertices[i]);
-            // }
-            // vh.AddTriangle(0, 1, 2);
-            // vh.AddTriangle(2, 3, 0);
-            // vh.AddTriangle(4, 5, 6);
-            // vh.AddTriangle(6, 7, 4);
         }
 
         public override void Rebuild(CanvasUpdate executing)
@@ -139,30 +96,7 @@ namespace JSSoft.UI
             // return;
             if (executing == CanvasUpdate.LatePreRender)
             {
-                if (m_CachedMesh == null)
-                {
-                    m_CachedMesh = new Mesh();
-                }
                 var rect = this.rectTransform.rect;
-                var vertices = new Vector3[8];
-                var uvs = new Vector2[8];
-                var colors = new Color32[8]
-                {
-                    TerminalColors.Green,
-                    TerminalColors.Green,
-                    TerminalColors.Green,
-                    TerminalColors.Green,
-                    TerminalColors.Black,
-                    TerminalColors.Black,
-                    TerminalColors.Black,
-                    TerminalColors.Black
-                };
-                var triangles = new int[12]
-                {
-                    0, 1, 2, 2, 3, 0,
-                    4, 5, 6, 6, 7, 4
-                };
-
                 var fontAsset = FontUtility.GetFontAsset(this.fontAsset, this.character);
                 if (fontAsset != null)
                 {
@@ -177,61 +111,70 @@ namespace JSSoft.UI
                     var x = (rect.x + rect.width / 2) - (glyphRect.width / 2);
                     var y = (rect.y + rect.height / 2) - (glyphRect.height / 2);
 
-                    vertices[0] = new Vector3(x, y, 0);
-                    vertices[1] = new Vector3(x, y + glyphRect.height, 0);
-                    vertices[2] = new Vector3(x + glyphRect.width, y + glyphRect.height, 0);
-                    vertices[3] = new Vector3(x + glyphRect.width, y, 0);
-                    vertices[4] = new Vector3(x, y, 0);
-                    vertices[5] = new Vector3(x, y + glyphRect.height, 0);
-                    vertices[6] = new Vector3(x + glyphRect.width, y + glyphRect.height, 0);
-                    vertices[7] = new Vector3(x + glyphRect.width, y, 0);
+                    this.vertices[0] = new Vector3(x, y, 0);
+                    this.vertices[1] = new Vector3(x, y + glyphRect.height, 0);
+                    this.vertices[2] = new Vector3(x + glyphRect.width, y + glyphRect.height, 0);
+                    this.vertices[3] = new Vector3(x + glyphRect.width, y, 0);
+                    this.vertices[4] = new Vector3(x, y, 0);
+                    this.vertices[5] = new Vector3(x, y + glyphRect.height, 0);
+                    this.vertices[6] = new Vector3(x + glyphRect.width, y + glyphRect.height, 0);
+                    this.vertices[7] = new Vector3(x + glyphRect.width, y, 0);
 
-                    uvs[0] = new Vector2(0, 0);
-                    uvs[1] = new Vector2(0, 1);
-                    uvs[2] = new Vector2(1, 1);
-                    uvs[3] = new Vector2(1, 0);
-                    uvs[4] = new Vector2(uv0.x, uv0.y);
-                    uvs[5] = new Vector2(uv0.x, uv1.y);
-                    uvs[6] = new Vector2(uv1.x, uv1.y);
-                    uvs[7] = new Vector2(uv1.x, uv0.y);
+                    this.uvs[0] = new Vector2(0, 0);
+                    this.uvs[1] = new Vector2(0, 1);
+                    this.uvs[2] = new Vector2(1, 1);
+                    this.uvs[3] = new Vector2(1, 0);
+                    this.uvs[4] = new Vector2(uv0.x, uv0.y);
+                    this.uvs[5] = new Vector2(uv0.x, uv1.y);
+                    this.uvs[6] = new Vector2(uv1.x, uv1.y);
+                    this.uvs[7] = new Vector2(uv1.x, uv0.y);
                     this.texture = texture;
                 }
                 else
                 {
-                    vertices[0] = new Vector3(rect.x + 100, rect.y, 0);
-                    vertices[1] = new Vector3(rect.x + 100, rect.y + rect.height, 0);
-                    vertices[2] = new Vector3(rect.x - 100 + rect.width, rect.y + rect.height, 0);
-                    vertices[3] = new Vector3(rect.x - 100 + rect.width, rect.y, 0);
-                    vertices[4] = new Vector3(rect.x + 200, rect.y, 0);
-                    vertices[5] = new Vector3(rect.x + 200, rect.y + rect.height, 0);
-                    vertices[6] = new Vector3(rect.x - 200 + rect.width, rect.y + rect.height, 0);
-                    vertices[7] = new Vector3(rect.x - 200 + rect.width, rect.y, 0);
+                    this.vertices[0] = new Vector3(rect.x + 100, rect.y, 0);
+                    this.vertices[1] = new Vector3(rect.x + 100, rect.y + rect.height, 0);
+                    this.vertices[2] = new Vector3(rect.x - 100 + rect.width, rect.y + rect.height, 0);
+                    this.vertices[3] = new Vector3(rect.x - 100 + rect.width, rect.y, 0);
+                    this.vertices[4] = new Vector3(rect.x + 200, rect.y, 0);
+                    this.vertices[5] = new Vector3(rect.x + 200, rect.y + rect.height, 0);
+                    this.vertices[6] = new Vector3(rect.x - 200 + rect.width, rect.y + rect.height, 0);
+                    this.vertices[7] = new Vector3(rect.x - 200 + rect.width, rect.y, 0);
 
-                    uvs[0] = new Vector2(0, 0);
-                    uvs[1] = new Vector2(0, 1);
-                    uvs[2] = new Vector2(1, 1);
-                    uvs[3] = new Vector2(1, 0);
-                    uvs[4] = new Vector2(0, 0);
-                    uvs[5] = new Vector2(0, 1);
-                    uvs[6] = new Vector2(1, 1);
-                    uvs[7] = new Vector2(1, 0);
+                    this.uvs[0] = new Vector2(0, 0);
+                    this.uvs[1] = new Vector2(0, 1);
+                    this.uvs[2] = new Vector2(1, 1);
+                    this.uvs[3] = new Vector2(1, 0);
+                    this.uvs[4] = new Vector2(0, 0);
+                    this.uvs[5] = new Vector2(0, 1);
+                    this.uvs[6] = new Vector2(1, 1);
+                    this.uvs[7] = new Vector2(1, 0);
                     this.texture = this.fontAsset?.atlasTexture;
                 }
-                m_CachedMesh.Clear();
-                m_CachedMesh.subMeshCount = 2;
-                m_CachedMesh.vertices = vertices;
-                m_CachedMesh.uv = uvs;
-                m_CachedMesh.colors32 = colors;
-                // m_CachedMesh.triangles = triangles;
-                m_CachedMesh.SetTriangles(new int[] { 0, 1, 2, 2, 3, 0}, 0);
-                m_CachedMesh.SetTriangles(new int[] { 4, 5, 6, 6, 7, 4}, 1);
+                this.Mesh.Clear();
+                this.Mesh.subMeshCount = 2;
+                this.Mesh.vertices = vertices;
+                this.Mesh.uv = uvs;
+                this.Mesh.colors32 = colors;
+                this.Mesh.SetTriangles(backgroundTriangles, 0);
+                this.Mesh.SetTriangles(foregroundTriangles, 1);
                 this.canvasRenderer.materialCount = 2;
-                
+
                 this.canvasRenderer.SetMaterial(this.material, 0);
                 this.canvasRenderer.SetMaterial(this.fontAsset?.material, 1);
-                this.canvasRenderer.SetMesh(m_CachedMesh);
-                // if (this.fontAsset?.material.
-                Debug.Log("1");
+                this.canvasRenderer.SetMesh(this.Mesh);
+            }
+        }
+
+        private Mesh Mesh
+        {
+            get
+            {
+                if (m_CachedMesh == null)
+                {
+                    m_CachedMesh = new Mesh();
+                }
+                return m_CachedMesh;
             }
         }
     }
