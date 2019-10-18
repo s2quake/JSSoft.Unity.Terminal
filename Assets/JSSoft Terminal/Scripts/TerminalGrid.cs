05f44c462efba25fcbcf7d6f65205c128e46884a
s2quake <s2quake@naver.com>
2019-10-18 19:41:28 +0900

    선택 관련 기능 개선중

--- Assets/JSSoft Terminal/Scripts/TerminalGrid.cs
+++ Assets/JSSoft Terminal/Scripts/TerminalGrid.cs
@@ -660,42 +660,54 @@ namespace JSSoft.UI
             var s1 = p1 < p2 ? p1 : p2;
             var s2 = p1 > p2 ? p1 : p2;
 
-            var row1 = this.rows[s1.Y];
-            if (row1.IsEmpty == true)
+            var cell1 = this.rows[s1.Y].Cells[s1.X];
+            var cell2 = this.rows[s2.Y].Cells[s2.X];
+            if (cell1.IsEnabled == true && cell2.IsEnabled == true)
             {
-                s1.X = this.ColumnCount;
+                return (s1, s2);
             }
-            else
+            else if (cell1.IsEnabled == true)
             {
-                var cell = row1.Cells[s1.X];
-                if (cell.IsEnabled == false)
+                var row2 = this.rows[s2.Y];
+                if (row2.IsEmpty == true)
                 {
-                    var j = Math.Max(0, s1.X - 6);
-                    for (var i = s1.X; i >= j; i--)
+                    s2.X = this.ColumnCount;
+                }
+                else
+                {
+                    var cell = row2.Cells[s2.X];
+                    if (cell.IsEnabled == false)
                     {
-                        var item = row1.Cells[i];
-                        if (item.IsEnabled == true)
+                        s2.X = this.ColumnCount;
+                    }
+                }
+            }
+            else if (cell2.IsEnabled == true)
+            {
+                var row1 = this.rows[s1.Y];
+                if (row1.IsEmpty == true)
+                {
+                    s1.X = this.ColumnCount;
+                }
+                else
+                {
+                    var cell = row1.Cells[s1.X];
+                    if (cell.IsEnabled == false)
+                    {
+                        var j = Math.Max(0, s1.X - 6);
+                        for (var i = s1.X; i >= j; i--)
                         {
-                            s1.X = i + 1;
-                            break;
+                            cell = row1.Cells[i];
+                            if (cell.IsEnabled == true)
+                            {
+                                s1.X = i + 1;
+                                break;
+                            }
                         }
                     }
                 }
             }
 
-            var row2 = this.rows[s2.Y];
-            if (row2.IsEmpty == true)
-            {
-                s2.X = this.ColumnCount;
-            }
-            else
-            {
-                var cell = row2.Cells[s2.X];
-                if (cell.IsEnabled == false)
-                {
-                    s2.X = this.ColumnCount;
-                }
-            }
             return (s1, s2);
         }
 
