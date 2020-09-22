1b8778b3878dcab1462b404e8d6251f47e9bcfdd
s2quake <s2quake@naver.com>
2020-09-22 22:13:34 +0900

    터미널 위치를 감추고 보이게 하는 컨트롤러 기능 개선

---
+++ Assets/Plugins/JSSoft.Terminal/Scripts/RectVisibleDirection.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using JSSoft.Terminal;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace JSSoft.Terminal
+{
+    public enum RectVisibleDirection
+    {
+        Left,
+
+        Top,
+
+        Right,
+        
+        Bottom,
+    }
+}
