b5cd441fcff0b0da05cb6df446057b1fbd3ae9a0
s2quake <s2quake@naver.com>
2020-12-05 21:03:12 +0900

    문자 정렬

--- Runtime/Scripts/Commands/DebugCommandAttribute.cs
+++ Runtime/Scripts/Commands/DebugCommandAttribute.cs
@@ -9,7 +9,7 @@
 //
 // Copyright (c) 2020 Jeesu Choi
 // E-mail: han0210@netsgo.com
-// Site : https://s2quake.github.io/u-terminal
+// Site  : https://s2quake.github.io/u-terminal
 // 
 ////////////////////////////////////////////////////////////////////////////////
 
