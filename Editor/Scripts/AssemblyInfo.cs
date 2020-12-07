2d76434a57a0d655fb9deced1fdfc3a6363f6d64
s2quake <han0210@netsgo.com>
2020-12-07 16:11:10 +0900

    버전 1.0.1로 수정 폰트 재출력

--- Editor/Scripts/AssemblyInfo.cs
+++ Editor/Scripts/AssemblyInfo.cs
@@ -24,8 +24,8 @@ using System.Reflection;
 #endif
 [assembly: AssemblyCopyright("Copyright (c) 2020 Jeesu Choi")]
 [assembly: AssemblyDescription("Terminal Editor for Unity")]
-[assembly: AssemblyFileVersion("1.0.0")]
-[assembly: AssemblyInformationalVersion("1.0.0")]
+[assembly: AssemblyFileVersion("1.0.1")]
+[assembly: AssemblyInformationalVersion("1.0.1")]
 [assembly: AssemblyProduct("JSSoft.Unity.Terminal.Editor")]
 [assembly: AssemblyTitle("JSSoft.Unity.Terminal.Editor")]
 [assembly: AssemblyVersion("1.0.0.0")]
